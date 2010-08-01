add9c629b000bf146243cf3ffba5afbf5554ba0b
riotopsys <riotopsys@yahoo.com>
2010-08-01 10:00:07 -0400

    reentry problem fixed loads extras and displays when avaliable

--- src/com/riotopsys/MALforAndroid/AnimeDetail.java
+++ src/com/riotopsys/MALforAndroid/AnimeDetail.java
@@ -1,7 +1,10 @@
 package com.riotopsys.MALforAndroid;
 
 import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
@@ -21,11 +24,21 @@ public class AnimeDetail extends Activity {
 	private TextView memberScore;
 	private TextView rank;
 	private TextView synopsis;
+	private IntentFilter intentFilter;
+	
+	private long id;
+	private Reciever rec;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.detail);
+		
+		intentFilter = new IntentFilter("com.riotopsys.MALForAndroid.FETCH_COMPLETE");
+		
+		rec = new Reciever();
+		
+		registerReceiver(rec, intentFilter);
 
 		title = (TextView) this.findViewById(R.id.detailTitle);
 		progress = (TextView) this.findViewById(R.id.detailProgress);
@@ -38,14 +51,16 @@ public class AnimeDetail extends Activity {
 		memberScore = (TextView) this.findViewById(R.id.detailMemberScore);
 		rank = (TextView) this.findViewById(R.id.detailRank);
 		synopsis = (TextView) this.findViewById(R.id.detailSynopsis);
-
-		MALSqlHelper openHelper = new MALSqlHelper(this.getBaseContext());
-		SQLiteDatabase db = openHelper.getReadableDatabase();
-
+		
 		Bundle b = getIntent().getExtras();
 
-		long id = b.getLong("id", 0);
+		id = b.getLong("id", 0);
 
+		display();
+		
+		/*MALSqlHelper openHelper = new MALSqlHelper(this.getBaseContext());
+		SQLiteDatabase db = openHelper.getReadableDatabase();
+		
 		String s = "select * from animeList where id = " + String.valueOf(id);
 
 		Cursor c = db.rawQuery(s, null);
@@ -83,7 +98,72 @@ public class AnimeDetail extends Activity {
 
 		}
 
+		db.close();*/
+	}
+	
+	private void display(){
+		MALSqlHelper openHelper = new MALSqlHelper(this.getBaseContext());
+		SQLiteDatabase db = openHelper.getReadableDatabase();
+		
+		String s = "select * from animeList where id = " + String.valueOf(id);
+
+		Cursor c = db.rawQuery(s, null);
+
+		if (c != null) {
+			if (c.moveToFirst()) {
+
+				title.setText(c.getString(1));
+				progress.setText(c.getString(6) + " of " + c.getString(4));
+				score.setText("Score: " + c.getString(7));
+				status.setText(c.getString(5));
+				type.setText(c.getString(2));
+				watchedStatus.setText(c.getString(8));
+
+				if (!c.isNull(10)) {
+					memberScore.setText("Member Score: "+c.getString(10));
+					rank.setText("Rank: " + c.getString(11));
+					synopsis.setText(new String(c.getBlob(12)));
+
+				} else {
+					memberScore.setText("");
+					rank.setText("");
+					synopsis.setText("");
+
+					Intent i = new Intent(this, MALManager.class);
+					i.setAction("com.riotopsys.MALForAndroid.FETCH_EXTRAS");
+					Bundle b = new Bundle();
+					b.putLong("id", id);
+					i.putExtras(b);
+					startService(i);
+				}
+
+			}
+			c.close();
+
+		}
+
 		db.close();
+	}
+	
+	@Override 
+	public void onPause(){
+		unregisterReceiver( rec );
+		super.onPause();
+	}
+	
+	@Override 
+	public void onResume(){
+		registerReceiver( rec, intentFilter );
+		super.onPause();
+	}
+	
+	private class Reciever extends BroadcastReceiver{
+
+		@Override
+		public void onReceive(Context arg0, Intent arg1) {
+			display();
+		}
+		
 	}
 
 }
